b4d5417e61518c02947ca95d06762cf08ed524c5
stiansigvartsen <stian_sigvartsen@hotmail.com>
2014-06-11 14:30:43 +0100

    Updated control.js to change how data brakes down

--- treemap/js/control.js
+++ treemap/js/control.js
@@ -10,9 +10,9 @@ $( document ).ready(function() {
 		// To do this change d.grand_parent, d.parent and d.child to the column titles in the order you wish to nest them, e.g. d.column_1, d.column_2 etc.
 		// NOTE: Your column titles cannot contain spaces.
 		var nested_data = d3.nest()
-			.key(function(d)  { return d.grand_parent; })
-			.key(function(d)  { return d.parent; })
-			.key(function(d)  { return d.child; })
+			.key(function(d)  { return d.Destination; })
+			.key(function(d)  { return d.Supplier_name; })
+			.key(function(d)  { return d.Ticket_class_description; })
 			.entries(csv_data);
 
 		// Creat the root node for the treemap
@@ -23,7 +23,7 @@ $( document ).ready(function() {
 		root.values = nested_data;
 
 		// Change the key names and children values from .next and add values for a chosen column to define the size of the blocks, e.g. a value
-		root = reSortRoot(root,"size_column");
+		root = reSortRoot(root,"Paid_fare");
 
 		// DEBUG
 		// $("#rawdata").html(JSON.stringify(root));
